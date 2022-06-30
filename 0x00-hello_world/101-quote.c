5304c13baab0a7d3549c65299deea21e95ef7879
AlehegnT <aletesfayem@gmail.com>
2022-06-30 10:33:19 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
 
 /**
- *  main - Entry point
- *  Return: Always 1 (Success)
+ *
+ * main - run all functions
+ *
+ * Description: Change standard error message
+ * Return: Return 1 and exit
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful
-		   \" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	int l;
+
+	l =
+	sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+
+	write(2,
+	"and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+	return (1);
 }
