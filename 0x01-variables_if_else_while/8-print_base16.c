e501a396d9b4cc8f86d87de2087e62e1f581ff4b
AlehegnT <aletesfayem@gmail.com>
2022-07-01 10:49:10 -0700

    My C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = '0';
+
+	while (b <= '9')
+	{
+		putchar(b);
+		b++;
+	}
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(\'n');
+	return (0);
+}
