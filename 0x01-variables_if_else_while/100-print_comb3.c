649f1fdbe619dd87c94a1232f00f511d0b804162
AlehegnT <aletesfayem@gmail.com>
2022-07-01 11:48:27 -0700

    My C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+	int d = '0';
+
+	while (c <= '9')
+	{
+		d = '0';
+		while (d <= '9')
+		{
+			putchar(f);
+			putchar(s);
+			if (s == '9' && f == '9')
+			{
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			s++;
+		}
+		f++;
+	}
+	putchar('\n');
+	return (0);
+}
