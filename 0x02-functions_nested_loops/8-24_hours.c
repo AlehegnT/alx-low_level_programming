4045b86ed153ee81a0c164761b7150865953cecc
AlehegnT <aletesfayem@gmail.com>
2022-07-05 07:09:34 -0700

    My - C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute in 24 hours
+ *
+ * Return:
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
