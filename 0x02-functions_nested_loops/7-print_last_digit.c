ddc51b85b5553f0d8e54256ab0ddea33593e4647
AlehegnT <aletesfayem@gmail.com>
2022-07-05 08:53:38 -0700

    My - C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_last_digit - write last digit
- * @n: integer
- * Return: value
+ * print_last_digit - print last digit of an integer
+ * @n: the integer to print the last digit of
+ * Return: Value of last digit
  */
 int print_last_digit(int n)
 {
