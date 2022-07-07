ba7806fbfa0551f557ad523dd1edba52f0f0f782
AlehegnT <aletesfayem@gmail.com>
2022-07-07 07:14:32 -0700

    my 0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - Find upper case
+ * @c: letter
+ *
+ * Return: 1 if c is uppercas, 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+		return (0);
+}
