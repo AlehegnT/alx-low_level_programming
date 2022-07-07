d635abdaa073d03dcce18ed120111634d597307d
AlehegnT <aletesfayem@gmail.com>
2022-07-07 10:05:54 -0700

    my 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,22 +7,29 @@
  */
 int main(void)
 {
-	int n = 1;
-
-	while (n <= 100)
-	{
-		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
-		else if (n % 3 == 0)
-			printf("Fizz ");
-		else if (n % 5 == 0)
-			printf("Buzz ");
-		else
-			printf("%d", n);
-		if (n != 100)
-			printf(" ");
-		n++;
-	}
-	printf("\n");
-	return (0);
+int n;
+for (n = 1; n < 101; n++)
+{
+if ((n % 3 == 0) && (n % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (n % 5 == 0)
+{
+if (n < 100)
+printf("Buzz ");
+else
+printf("Buzz");
+}
+if (!((n % 3 == 0) || (n % 5 == 0)))
+{
+printf("%d ", n);
+}
+}
+printf("\n");
+return (0);
 }
