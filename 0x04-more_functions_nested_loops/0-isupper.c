701f24ae04e96d6fca13b06d55993c14c689022f
AlehegnT <aletesfayem@gmail.com>
2022-07-07 09:45:11 -0700

    my 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 /**
- * _isupper - Find upper case
- * @c: letter
+ * _isupper - Find if a given letter is upper case
+ * @c: the given letter
  *
- * Return: 1 if c is uppercas, 0 if not
+ * Return: Return 1 if c is uppercase, 0 if otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-		return (0);
+	return (0);
 }
